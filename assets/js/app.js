a133bec8f87372e0541e6c0c7350565d8f4b41e9
Priya Garg <gupta.ria100@gmail.com>
2020-12-06 23:59:32 -0600

    adding some extra features

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,15 +12,3 @@ function scrollToProjects() {
   window.scrollTo(0, 900);
 }
 
-/* Set the width of the side navigation to 250px */
-function openNav() {
-  sidenav.style.display = "flex ";
-  sidenav.style.transition = "ease-in-out 1s;";
-  closeBtn.style.display = "block";
-  document.getElementById("mySidenav").style.width = "250px";
-}
-
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-}
